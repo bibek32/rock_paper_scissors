2dbc5e681c2bb1b87e308d47916f18d51c2da105
Bibek <bibeksilwal.32@gmail.com>
2025-02-18 15:00:48 +0000

    Created just win, loos or draw

--- script/game.js
+++ script/game.js
@@ -1,58 +1,63 @@
-console.log('This is game of rock paper scissors');
+const options = document.querySelectorAll('.option');
 
-function getComputerChoice(){
-    const comp = Math.random();
+options.forEach((option) => {
+  option.addEventListener('click', () => {
+    let playerChoice = option.id;
+    let computerChoice = getComputerChoice();
+    let result = getResult(playerChoice, computerChoice);
+    displayResult(result, playerChoice, computerChoice);
+  });
+});;
 
-    //For Rock
-    if(comp < 0.33)
-        return 'rock';
+function displayResult(result, playerChoice, computerChoice) {
+  let resultElement = document.getElementById('result');
+  let choiceElement = document.getElementById('choice')
+  choiceElement.innerText = `The Computer choose ${computerChoice} and you choosed ${playerChoice}`;
+  if (result === 'd')
+  resultElement.innerText = `The result is draw`;
+  else if (result === 'p')
+    resultElement.innerText = "The player Wins";
+  else if (result === 'c')
+    resultElement.innerText = "The computer Wins";
+}
 
-    //For Paper
-    else if(comp < 0.66)
-        return 'paper';
+function getComputerChoice() {
+  let computerChoice = '';
+  let random = Math.random();
+  if (random < 0.34)
+    computerChoice = 'rock';
+  else if (random <= 0.67)
+    computerChoice = 'paper';
+  else
+    computerChoice = 'scissor';
+  return computerChoice;
+}
 
-    //For Scissors
+function getResult(playerChoice, computerChoice) {
+  let result = '';
+  if (playerChoice === computerChoice)
+    result = 'd';
+
+  else if (playerChoice === 'rock') {
+    if (computerChoice === 'paper')
+      result = 'c';
     else
-        return 'scissors';
+      result = 'p';
+  }
+
+  else if (playerChoice === 'paper') {
+    if (computerChoice === 'scissor')
+      result = 'c';
+    else
+      result = 'p';
+  }
+
+  else if (playerChoice === 'scissor') {
+    if (computerChoice === 'rock')
+      result = 'c';
+    else
+      result = 'p';
+  }
+
+  return result;
 }
-
-let computerChoice = getComputerChoice();
-
-function getUserChoice(){
-    let user = prompt('Enter your choice: rock, paper or scissors');
-    return user;
-}
-
-let userChoice = getUserChoice();
-
-function determineWinner(computerChoice, userChoice){
-    if(computerChoice === userChoice)
-        return 'The game is a tie!';
-
-    if(computerChoice === 'rock'){
-        if(userChoice === 'paper')
-            return 'User wins!';
-        else
-            return 'Computer wins!';
-    }
-
-    (computerChoice === 'rock') ? (userChoice === 'paper') ? 'User wins!' : 'Computer wins!';
-
-
-
-    if(computerChoice === 'paper'){
-        if(userChoice === 'scissors')
-            return 'User wins!';
-        else
-            return 'Computer wins!';
-    }
-
-    if(computerChoice === 'scissors'){
-        if(userChoice === 'rock')
-            return 'User wins!';
-        else
-            return 'Computer wins!';
-    }
-}
-
-console.log(determineWinner(computerChoice, userChoice));
